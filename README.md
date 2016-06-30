c23fab01c0ac7335fceab20b20bf399d29b9d7fb
Dennis Plöger <dennis.ploeger@hosea.office.getit.de>
2016-06-30 14:33:46 +0200

    Added first version

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# packer-oracle
-Oracle database templates for Packer
+# Introduction
+
+These are the templates for various publicly available Vagrant boxes 
+providing an Oracle database.
+
+The template oracle.json currently has these builders available:
+
+* VirtualBox provider:
+    * [oracle-XE-11.2-x86_64](https://atlas.hashicorp.com/dploeger/boxes/oracle-XE-11.2-x86_64): Oracle XE 11.2 on 64 bit Cent OS 7
+    * [oracle-XE-11.2-x86_64-fixed](https://atlas.hashicorp.com/dploeger/boxes/oracle-XE-11.2-x86_64-fixed): Oracle XE 11.2 on 64 bit 
+    Cent OS 7 (will temporarily create a large file when deployed to optimize
+     disk performance)
+  
+The definitions are based on the works of https://github.com/stlhrt/vagrant-boxes and 
+converted to Packer.
+
+# Building
+
+To build the boxes, install [packer](https://packer.io) and
+ run from the base path:
+  
+    packer build oracle.json
+
+This will build the boxes and generate Vagrant boxes in the current directory.
+
+To build only a specific box, you can use the "-only"-flag:
+
+    packer build -only=oracle-XE-11.2-x86_64 oracle.json 
---
+++ scripts/oracle.sh
@@ -0,0 +1,40 @@
+# Installing the oracle XE DB
+
+echo "Disabling IPv6 ####################################################"
+# Disable ipv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+# Disable firewall
+service iptables save
+service iptables stop
+chkconfig iptables off
+
+# Unzip first
+yum -y install unzip bc libaio flex
+mkdir /tmp/oracle
+unzip /tmp/oracle/oracle-xe-11.2.0-1.0.x86_64.rpm.zip -d /tmp/oracle
+
+# Install and configure oracle
+
+cd /tmp/oracle/Disk1
+rpm -ivh oracle-xe-11.2.0-1.0.x86_64.rpm
+/etc/init.d/oracle-xe configure responseFile=/tmp/oracle/xe.rsp
+
+# Add oracle environments to vagrant and oracle user
+
+echo ". /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh" >> /home/vagrant/.bash_profile
+su - -c 'echo ". /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh" >> ~/.bash_profile' oracle
+
+# Run database init script
+
+source /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
+echo exit | sqlplus -S system/manager@localhost/xe @/tmp/oracle/db_init.sql
+
+# Cleanup
+rm -rf /tmp/oracle
+
+# Reboot
+service sshd stop && reboot
